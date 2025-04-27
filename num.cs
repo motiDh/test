100a01d41a4f09cbc64b50067e7b207e8b820e49
motiDh <mtdh1996@gmail.com>
2025-04-27 10:47:51 +0330

    Add files via upload

---
+++ num.cs
@@ -0,0 +1,16 @@
+﻿class Calculator
+{
+    static void Main()
+    {
+        Console.Write("first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Sum: {num1 + num2}");
+        Console.WriteLine($"Difference: {num1 - num2}");
+        Console.WriteLine($"Product: {num1 * num2}");
+        Console.WriteLine($"Quotient: {num1 / num2}");
+    }
+}
